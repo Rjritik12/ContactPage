0e3ac7d0f32085a66fb20774cd755b817908b630
ritik jaiswal <110723218+Rjritik12@users.noreply.github.com>
2023-07-18 20:44:06 +0530

    Rename contact-page.js to script.js

--- contact-page.js
+++
@@ -1,1 +0,0 @@
-alert("Hello! Take some look of my code and if u like it dont forget to upvote.") 
---
+++ script.js
@@ -0,0 +1,1 @@
+alert("Hello! Take some look of my code and if u like it dont forget to upvote.") 
